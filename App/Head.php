119e959f965275371e1ac27c35804bdb9c9146f0
Ricklocura <none>
2022-12-10 01:38:53 +0000

    Adicionadas Meta Tags OpenGraph

--- App/Head.php
+++ App/Head.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title" content="20 Questões" />
+    <meta property="og:image" content="https://i.ibb.co/bvcr3V0/donkey-silhouette-1445006716-Ws8-removebg-preview.png" />
+    <meta property="og:description" content="20 Questãoes de vida e lógica." />
     <title>
         <?php
         $pageName = rtrim(basename($_SERVER['PHP_SELF']), '.php');
