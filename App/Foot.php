a85a7155b57c063bfa90e83032b0a0fd9fc2aa5f
Ricklocura <none>
2022-12-10 01:43:03 +0000

    Remover marca d'água 000webhost

--- App/Foot.php
+++ App/Foot.php
@@ -2,4 +2,10 @@
 <footer class="text-white fixed-bottom d-flex justify-content-center align-items-center" style="background-color: rgba(0, 0, 0, .9);">
     <p class="b-0 p-0">Copyright © 2022 - 2666 Valdívia Inc. | All Rights Reserved.</p>
 </footer>
+<script type="text/javascript">
+    window.addEventListener('load', function() {
+        $("a[href*='000webhostapp']")[0].parentElement.remove()
+    })
+</script>
+
 </html>
